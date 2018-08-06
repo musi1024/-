c9bccca653ef94e30608e5434077763300e654cb
musi <957028613@qq.com>
2018-08-06 23:41:37 +0800

    更新

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(click).on('click', function(e) {
     $(popover).toggle()
-    $(document).one('mousedown', function(e) {
+    $(document).on('mousedown', function(e) {
         var $e = $(e.target)
         if (!$e.is($(click)) && !$e.is($(popover))) {
             $(popover).hide()
